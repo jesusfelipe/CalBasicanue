8f1a1c34bc35346c86b3f359e8a08889fa2276e9
jesus <jesus_feli@yahoo.es>
2019-05-16 23:38:35 +0200

    creación de códigos de botones

--- CalBasica/Controller/Home/HomeViewController.swift
+++ CalBasica/Controller/Home/HomeViewController.swift
@@ -10,21 +10,51 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - Controles
+    
+    // etiquetas
+    
+    @IBOutlet weak var etiqnumero1: UILabel!
+    @IBOutlet weak var etiqnumero2: UILabel!
+    @IBOutlet weak var etiqresultado: UILabel!
+    
+    // Cajas de texto
+    @IBOutlet weak var cajanumero1: UITextField!
+    @IBOutlet weak var cajanumero2: UITextField!
+    @IBOutlet weak var cajaresultado: UITextField!
+    
+    // Botones
+    @IBOutlet weak var botonsuma: UIButton!
+    @IBOutlet weak var botonresta: UIButton!
+    @IBOutlet weak var botonmulti: UIButton!
+    @IBOutlet weak var butonraiz: UIButton!
+    
+    // MARK: - Inicializacion
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Ciclo de vida
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Acción Botones
+    
+    @IBAction func actbotonsuma(_ sender: UIButton) {
     }
-    */
+    @IBAction func actbotonresta(_ sender: UIButton) {
+    }
+    @IBAction func actbotonmulti(_ sender: UIButton) {
+    }
+    @IBAction func actbotonraiz(_ sender: UIButton) {
+    }
+    
 
 }
