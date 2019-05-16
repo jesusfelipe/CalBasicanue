19fd498b331c4a91a6a5e0efec2470d697313669
jesus <jesus_feli@yahoo.es>
2019-05-16 23:08:39 +0200

    Nuevos ficheros añadidos

---
+++ CalBasica/Util/UIButtonExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIButtonExtension.swift
+//  CalBasica
+//
+//  Created by Jesús Felipe Vozmediano Vallejo on 16/05/2019.
+//  Copyright © 2019 Jesús Felipe Vozmediano Vallejo. All rights reserved.
+//
+
+import UIKit
+
+class UIButtonExtension: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
